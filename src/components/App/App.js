877ce6a83da02ab9df42ce83e6d5cb7771fdc54f
khuephamy <khuephumy19@gmail.com>
2023-04-15 12:46:30 -0500

    header styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Header } from '../Header/Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
- 
+    <div className="app">
+      <Header/>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React from "react";
+import logo from "../../images/logo.svg";
+import carticon from '../../images/carticon.svg';
+import "./Header.css";
 
 export function Header() {
-  
-
   return (
-   <header className='header'>
-    <div className="header__container">
+    <header className="header">
+      <div className="header__container">
+        <img src={logo} className="header__logo" />
 
-    </div>
-   </header>
-  )
+        <div className="header__co-container">
+          <div className="header__search-wrapper">
+            <input
+              className="header__search-input"
+              type="text"
+              name="search"
+              placeholder="Search..."
+            />
+            <button type="button" className="header__search-button" />
+          </div>
+
+          <div className="header__menu-wrapper">
+            <button className="header__button">Account</button>
+            <button className="header__button">Orders</button>
+            <button className="header__button">
+              <img src={carticon}/>
+              Cart
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
