a268e160f4e235325ace85840d04c069230fa189
khuephamy <khuephumy19@gmail.com>
2023-04-15 12:01:08 -0500

    init 0.1

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export function Header() {
+  
+
+  return (
+   <header className='header'>
+    <div className="header__container">
+
+    </div>
+   </header>
+  )
+}
