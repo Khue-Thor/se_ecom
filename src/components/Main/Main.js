6fb7a8420efcc82ce5019e7755a2728ea64ab80a
khuephamy <khuephumy19@gmail.com>
2023-04-16 13:08:59 -0500

    update some styles

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,23 +1,22 @@
 import React from "react";
 import { RecommendProducts } from "../RecommendProducts/RecommendProducts";
 import { ItemCard } from "../ItemCard/ItemCard";
-import "./Main.css"
+import "./Main.css";
 
 export function Main() {
   return (
     <main className="main">
-       
       <div className="main__container">
-      <RecommendProducts/>
-       <ul className="main__item-lists">
-       <ItemCard/>
-       <ItemCard/>
-       <ItemCard/>
-       <ItemCard/>
-       <ItemCard/>
-       <ItemCard/>
-       </ul>
+        <RecommendProducts />
+        <ul className="main__item-lists">
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+        </ul>
       </div>
     </main>
-  )
+  );
 }
