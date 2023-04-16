20ca202afa80c33b74afd3266c499c4cb0ee88bb
khuephamy <khuephumy19@gmail.com>
2023-04-16 11:27:19 -0500

    feature/ add and check out btn

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -12,7 +12,9 @@ export function ItemCard() {
             Upgraded, Anker Soundcore Bluetooth Speaker with IPX5 Waterproof, Stereo Sound, 24H
             Playtime, Portable Wireless Speaker for iPhone, Samsung and More
           </p>
-          <p className="card__price">$99.99</p>
+          <p className="card__price">price: <span className="card__price-number">$99.9</span></p>
+          <button type="button" className="card__add-button card__button">Add To Cart</button>
+          <button type="button" className="card__add-checkout card__button">Check Out</button>
         </div>
       </div>
     </div>
