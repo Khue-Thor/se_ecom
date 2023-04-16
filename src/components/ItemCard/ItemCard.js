5f9cf99a06a72c58ef6004467c09feb2be800b42
khuephamy <khuephumy19@gmail.com>
2023-04-15 20:06:07 -0500

    update RecommendProducts container heigth

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -1,19 +1,20 @@
 import React, { useContext } from "react";
 import "./ItemCard.css";
-import {speaker} from "../../utils/assests"
+import { speaker } from "../../utils/assests";
 
 export function ItemCard() {
-
   return (
-    <div className="card" >
+    <div className="card">
       <div className="card__content">
-        <img className="card__image"  src={speaker}/>
+        <img className="card__image" src={speaker} />
         <div className="card__detail">
-          <p className="card__description">Upgraded, Anker Soundcore Bluetooth Speaker with IPX5 Waterproof, Stereo Sound, 24H Playtime, Portable Wireless Speaker for iPhone, Samsung and More</p>
+          <p className="card__description">
+            Upgraded, Anker Soundcore Bluetooth Speaker with IPX5 Waterproof, Stereo Sound, 24H
+            Playtime, Portable Wireless Speaker for iPhone, Samsung and More
+          </p>
           <p className="card__price">$99.99</p>
         </div>
       </div>
-     
     </div>
   );
 }
